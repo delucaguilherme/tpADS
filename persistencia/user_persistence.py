4b163e734bfef23d2efdc9cd722d0214fb3df17d
Henrique Reisen Zanotti <91632289+henriqrz@users.noreply.github.com>
2023-09-20 18:41:43 -0300

    comentario persistencia

--- persistencia/user_persistence.py
+++ persistencia/user_persistence.py
@@ -2,4 +2,4 @@ from modelo.user import User
 
 class User_persistence:
     def __init__(self, usuarios):
-        self.usuarios = usuarios
+        self.usuarios = usuarios #lista de usuarios? carrega quando inicia o programa?
